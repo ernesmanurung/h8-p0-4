5f60f4c7e848af816c49695a0be317f8f3650056
ernest <ernesmanurungg@gmail.com>
2018-10-13 16:09:16 +0700

    exercise

---
+++ exercise4_cariModus.js
@@ -0,0 +1,40 @@
+function cariModus(arr){
+  var elemen = 0;
+  var count = 0;
+  var tamp = 0;
+  
+  for (var i = 0; i < arr.length; i++){
+    
+    for(var j = 0; j < arr.length; j++){
+
+      if(arr[i] == arr[j] && i !== j){
+        tamp++;
+
+        if(tamp > count){
+          count = tamp;
+          elemen = i;
+        }
+      }
+    }
+  }
+
+if(count === 0){
+  return -1;
+}
+
+var angka = 0;
+for(var k = 0; k < arr.length; k++){
+  angka += arr[k];
+  if(angka / arr.length === arr[k]){
+    return -1
+  }
+}
+return arr[elemen];
+}
+
+ // TEST CASES
+  console.log(cariModus([10, 4, 5, 2, 4])); // 4
+  console.log(cariModus([5, 10, 10, 6, 5])); // 5
+  console.log(cariModus([10, 3, 1, 2, 5])); // -1
+  console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
+  console.log(cariModus([7, 7, 7, 7, 7])); // -1
